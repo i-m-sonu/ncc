70da150e01c5a378bf2b69b684f18a517eb350dc
i-m-sonu <2237027.cse.cec@cgc.edu.in>
2024-09-16 23:05:40 +0530

    first committ

---
+++ src/app/pages/About.js
@@ -0,0 +1,56 @@
+import Image from "next/image";
+import React from "react"
+
+const About = () => {
+  return (
+   <>
+      <h2>INTRODUCTION OF NCC</h2>
+      <ul>
+        <li>
+          NCC is a voluntary organization that recruits cadets from high school,
+          colleges and universities all over India.
+        </li>
+        <li>
+          Recruits students between the age group of 13.5 years to 26 years.
+        </li>
+        <li>
+          The cadets are given basic military training in small arms and new
+          institutional training emphasis on Life Skill which is the need of the
+          hour.
+        </li>
+        <li>The NCC in India was formed in 1948.
+        </li>
+        <li> Old name of NCC: Universities Officers Training Corps (UOTC)
+        </li>
+        <Image src="/image.png" width={500} height={500} alt="Picture of the author"/>
+      </ul>
+      <h2>VISION & MISSION OF NCC
+      </h2>
+      <ul>
+        <li>
+           
+         Vision: Empower volunteer youth to become potential leaders and responsible citizen of the country
+           
+        </li>
+        <li>Mission: To develop leadership and character qualities, mould discipline and nurture social integration and cohesion through multi-faced programmes conducted in a military environment. 
+        </li>
+      </ul>
+      <h2>NATIONAL CADET CORPS (NCC SECOND LINE OF DEFENCE)
+      </h2>
+      
+        <ul>
+            <li>
+            NCC is the second line of defence, as it provides a large number of trained young people who could be employed for the defence of the country.
+            </li>
+            <li>
+            NCC is the second line of defence, as it provides a large number of trained young people who could be employed for the defence of the country.
+            </li>
+            <li>
+            NCC is the second line of defence, as it provides a large number of trained young people who could be employed for the defence of the country.
+            </li>
+
+     </>
+  );
+};
+
+export default About;
